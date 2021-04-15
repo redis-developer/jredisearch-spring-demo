f950567cbcd21606664047007071c85184d9c9cb
Brian Sam-Bodden <bsbodden@integrallis.com>
2021-04-14 23:29:48 -0700

    demo: remove impl, add fill in the blanks placeholders

--- src/main/java/com/redislabs/edu/redisearch/demo/RediSearchApplication.java
+++ src/main/java/com/redislabs/edu/redisearch/demo/RediSearchApplication.java
@@ -13,15 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.redisearch.Query;
-import io.redisearch.Schema;
 import io.redisearch.SearchResult;
 import io.redisearch.Suggestion;
 import io.redisearch.client.Client;
-import io.redisearch.client.IndexDefinition;
-import io.redisearch.client.SuggestionOptions;
 import lombok.RequiredArgsConstructor;
-import redis.clients.jedis.exceptions.JedisDataException;
 
 @SpringBootApplication
 public class RediSearchApplication {
@@ -29,38 +24,14 @@ public class RediSearchApplication {
   @Bean
   CommandLineRunner createSearchIndices(JedisConnectionFactory cf) {
     return args -> {
-      var searchIndex = "articles-idx";
-
-      try (var client = new Client(searchIndex, cf.getHostName(), cf.getPort())) {
-        var sc = new Schema() //
-            .addSortableTextField("title", 1.0) //
-            .addSortableNumericField("price");
-        var def = new IndexDefinition().setPrefixes("com.redislabs.edu.redisearch.demo.Article");
-        client.createIndex(sc, Client.IndexOptions.defaultOptions().setDefinition(def));
-      } catch (JedisDataException jde) {
-        // ignore - index already exists
-      }
+      // STEP 1. Create Search Index for Articles
     };
   }
 
   @Bean
   CommandLineRunner createAutoCompleteSuggestions(JedisConnectionFactory cf, RedisTemplate<String, String> template) {
     return args -> {
-      var autoCompleteKey = "author-auto-complete";
-      if (!template.hasKey(autoCompleteKey)) {
-        var authorKeySet = "com.redislabs.edu.redisearch.demo.Author";
-        var authorHashPrefix = "com.redislabs.edu.redisearch.demo.Author:";
-
-        try (var client = new Client(autoCompleteKey, cf.getHostName(), cf.getPort())) {
-          var authorIds = template.opsForSet().members(authorKeySet);
-          authorIds.forEach(authorId -> {
-            var authorHashKey = authorHashPrefix + authorId;
-            String authorName = template.opsForHash().get(authorHashKey, "name").toString();
-            var suggestion = Suggestion.builder().str(authorName).score(1).build();
-            client.addSuggestion(suggestion, true);
-          });
-        }
-      }
+      // STEP 3. Create Auto Complete Suggestion for Author Names
     };
   }
 
@@ -83,19 +54,16 @@ class ArticlesController {
       @RequestParam(defaultValue = "-1") Double minPrice, //
       @RequestParam(defaultValue = "-1") Double maxPrice) {
     try (var client = new Client("articles-idx", cf.getHostName(), cf.getPort())) {
-      var q = new Query(query);
-      if (minPrice != -1 && maxPrice != -1) {
-        q.addFilter(new Query.NumericFilter("price", minPrice, maxPrice));
-      }
-      q.returnFields("title", "price");
-      return client.search(q);
+      // STEP 2. Implement full-text search
+      return null;
     }
   }
 
   @GetMapping("/authors")
   Collection<Suggestion> authorAutoComplete(@RequestParam(name = "q") String query) {
     try (var client = new Client("author-auto-complete", cf.getHostName(), cf.getPort())) {
-      return client.getSuggestion(query, SuggestionOptions.builder().build());
+      // STEP 4. Implement auto-complete for author Names
+      return null;
     }
   }
 }
